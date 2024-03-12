ac297c07928d68504f09374159a9f9efadf81d1a
Kristen Simmons <kristen_simmons@ymail.com>
2024-03-12 12:43:24 -0500

    finishing new form

--- js/app.js
+++ js/app.js
@@ -65,6 +65,9 @@ function renderHeaderRow(tableElem){
   totalCell.textContent = 'Location Totals';
 }
 
+const headerRow = document.createElement('tr');
+tableElem.appendChild(headerRow);
+
 CookieStand.prototype.render = function() {
   const row = document.createElement('tr');
   tableElem.appendChild(row);
@@ -85,8 +88,40 @@ CookieStand.prototype.render = function() {
   totalDailyCell.textContent = this.dailyTotalSum;
 };
 
-const headerRow = document.createElement('tr');
-tableElem.appendChild(headerRow);
+const newForm = document.getElementById('addNewStore');
+function handleSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+}
+
+newForm.addEventListener('submit', handleSubmit,
+  function (event) {
+    event.preventDefault();
+    const newLocation = event.target.newLocation.value;
+    const newMinCust = parseInt(event.target.newMinCust.value);
+    const newMaxCust = parseInt(event.target.newMaxCust.value);
+    const newAvg = parseInt(event.target.newAvg.value);
+
+    const newData = newData(newLocation, newMinCust, newMaxCust,newAvg);
+    console.log(newLocation);
+    newData.estimateSales();
+    newForm.reset();
+    newData.render();
+    cities.push(newData);
+    renderNewRow();
+    document.getElementById('footerRowTotal').remove();
+    renderFooterRow(tableElem);
+  });
+
+function renderNewRow(){
+  let tnew=document.querySelector('tnew');
+  if(tnew){
+    tnew.innerHTML = "";
+  } else {
+  tnew = document.createElement('new');
+  tableElem.appendChild(tnew);
+  }
+}
 
 function renderFooterRow(tableElem){
   const row = document.createElement('tfoot');
@@ -109,16 +144,5 @@ function renderFooterRow(tableElem){
   salesForAllLocations.textContent = grandTotal;
 }
 
-console.log(grandTotal);
 renderTable();
-
-const form = document.getElementById('addNewStore');
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const newData = event.target.newLocation.value;
-  console.log(newData);
-  event.target.reset();
-}
-
-form.addEventListener('submit', handleSubmit);
+console.log(grandTotal);
